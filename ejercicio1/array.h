ccf9a06a239ba35650d38e92aeaa7f95fb58398e
apmpm <alejandrpm@gmail.com>
2020-02-13 22:54:31 +0100

    ejercicio1 estructura

---
+++ ejercicio1/array.h
@@ -0,0 +1,13 @@
+#ifndef ARRAY_HPP
+#define  ARRAY_HPP
+
+int init(char *nombre, int N);
+
+int set(char *nombre, int i, int valor);
+
+int get(char *nombre, int i, int *valor);
+
+int destroy(chat *nombre);
+
+
+#endif
