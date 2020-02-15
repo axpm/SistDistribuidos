c5808de63a76003200885ab4c7dbb05d60fdf9aa
apmpm <alejandrpm@gmail.com>
2020-02-15 19:45:46 +0100

    el set

--- ejercicio1/array.c
+++ ejercicio1/array.c
@@ -10,7 +10,7 @@ int init(char *name, int N){
 }
 
 int set(char *name, int i, int value){
-  return 0;
+  // return 0;
 
   //creamos las peticiones y respuestas
   struct petition p;
@@ -54,7 +54,47 @@ int set(char *name, int i, int value){
 }
 
 int get(char *name, int i, int *value){
-  return 0;
+  // return 0;
+
+  //creamos las peticiones y respuestas
+  struct petition p;
+  struct reply r;
+
+  //abrimos las colas de cliente y server
+  char * clientName = (char *) getpid(); //Se le da un nombre para la cola del cliente
+  int cq = mq_open( clientName , O_CREATE);
+  if (cq == -1)
+    return cq;
+  int sq = mq_open("NOMBRE DE COLA SERVER"); //POR DEFINIR
+  if (sq == -1){
+    mq_close(cq);
+    mq_unlink(clientName);
+    return sq;
+  }
+
+  //Rellenamos la petición
+  p.op = SET_OP;
+  strcpy(p.name, name);
+  strcpy(p.client_queue, clientName);
+  p.i = i;
+
+  //Enviamos la petición
+  mq_send(sq, &p, ....);
+
+  e = mq_receive(cq, &r, ...)
+//--------------NO MUY SEGURO DE LO SIGUIENTE--------
+
+  if (e == -1){
+    mq_close(cq);
+    mq_close(sq);
+    mq_unlink(clientName);
+    return e;
+  }
+    *value = r.value;
+    mq_close(cq);
+    mq_close(sq);
+    mq_unlink(clientName);
+    return r.error;
 }
 
 int destroy(chat *name){
