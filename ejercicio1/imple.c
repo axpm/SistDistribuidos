d914be57bfe522d88c476a67e14af0cadff3d018
l4ur4s4nch <laura.sanchezcerro@gmail.com>
2020-02-18 18:59:02 +0100

    Correcciones operaciones

--- ejercicio1/imple.c
+++ ejercicio1/imple.c
@@ -68,13 +68,22 @@ void insert(List *l, int *vector, char * name){
 int init(char *name, int N){
   int cod_error = 1;
   //Si se pasan argumentos erróneos, devuelve -1
-  if (name == NULL || N < 0){
+  if (name == NULL || N <= 0){
     fprintf(stderr, "%s\n", "Invalid arguments for the new array" );
     return -1;
   }
 
   //---------OJO-------------
   List aux = server;// Pseudocódigo
+  //mirar que este vacía arriba
+  if (server == NULL){
+    *List newElement = (struct listElement)malloc(sizeof(struct listElement));
+    strcpy(newElement->name, name);
+    int vector[N];
+    newElement->vector = vector;
+    newElement->next = NULL;
+    server = newElement;
+  }
 
   while(aux->next != NULL){
     //Si hay un elemento con el mismo nombre y distinto tamaño error
@@ -82,25 +91,28 @@ int init(char *name, int N){
     ////NO SÉ SI ESTÁ BIEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if ( (strcmp(aux->name, name) == 0 ) && ( (int) (sizeof(*server->vector)/sizeof(int)) != N ) )
       return -1;
-    if ( (strcmp(aux->name, name)  == 0) && ( (int) (sizeof(*server->vector)/sizeof(int)) == N ) )
+    if ( (strcmp(aux->name, name) == 0) && ( (int) (sizeof(*server->vector)/sizeof(int)) == N ) )
       return 0;
-
-
     //---------OJO-------------
     aux = aux->next;// Pseudocódigo
   }
 
+  //int vector[N];
+  //insert(&server, vector, name);
+  //EN vez de llamar al insert
+  *List newElement = (struct listElement)malloc(sizeof(struct listElement));
+  strcpy(newElement->name, name);
   int vector[N];
-  insert(&server, vector, name);
-
+  newElement->vector = vector;
+  newElement->next = NULL;
+  server = newElement;
   return cod_error;
 }
 
 int set(char *name, int i, int value){
   //return 0;
   List aux = server;
-
-  if (i < 0)
+  if (server == NULL || i < 0)
     return -1;
 
   while (strcmp(aux->name, name) != 0) { //Compara que los nombres sean iguales
@@ -115,10 +127,11 @@ int set(char *name, int i, int value){
     return -1;
 
   //Lo ha encontrado
-  for (int j = 0; j<N; j++){
-    if(i == j)
+  //for (int j = 0; j<N; j++){
+    //if(i == j)
+    //Sobraría porque vamos a una posición concreta
       aux->vector[i] = value;
-  }
+  //}
   return 0;
 }
 
@@ -128,8 +141,8 @@ int get(char *name, int i, int *value){
   //return 0;
   List aux = server;
 
-  if (i < 0)
-  return -1;
+  if (server == NULL || i < 0)
+    return -1;
 
   while (strcmp(aux->name, name) != 0) { //Compara que los nombres sean iguales
     aux = aux->next;
@@ -143,35 +156,56 @@ int get(char *name, int i, int *value){
   return -1;
 
   //Lo ha encontrado
-  for (int j = 0; j<N; j++){
-    if(i == j)
+//for (int j = 0; j<N; j++){
+  //  if(i == j)
       *value = aux->vector[i];
-  }
+  //}
   return 0;
 }
 
-
-
+// int destroy(char *name){
+//   // return 0;
+//   //Encontramos vector name, reasignamos punteros next y frees de vector name
+//
+//   List aux = server;
+// while (strcmp(aux->next->name, name) != 0) { //Compara que los nombres sean iguales
+//   aux = aux->next;
+//   if (aux->next == NULL){
+//     fprintf(stderr, "%s\n", "Not existing element");
+//     return -1;
+//   }
+// }
+// // El que queremos eliminar es aux->next, por tanto queremos que aux apunte al next del next
+// aux->next = aux->next->next;
+// aux = aux->next;
+// free(aux); //Hacemos free de la posición actual
+//
+// return 1;
+//}
 
 int destroy(char *name){
   // return 0;
-
-  //Encontramos vector name, reasignamos punteros next y frees de vector name
+  if (server == NULL)
+    return -1;
 
   List aux = server;
-
-  while (strcmp(aux->next->name, name) != 0) { //Compara que los nombres sean iguales
-    aux = aux->next;
-    if (aux->next == NULL){
-      fprintf(stderr, "%s\n", "Not existing element");
-      return -1;
-    }
+  List prev = server; //Necesitamos un previo para hacer el borrado
+  //Si es el primero
+  if (strcmp(aux->name, name) != 0) {
+    server = aux->next;
+    free(aux);
   }
-  // El que queremos eliminar es aux->next, por tanto queremos que aux apunte al next del next
-  aux->next = aux->next->next;
-  aux = aux->next;
-  free(aux); //Hacemos free de la posición actual
-
-  return 1;
-
+  else { //Si no lo es
+    while (strcmp(aux->name, name) != 0) { //Compara que los nombres sean iguales
+      prev = aux;
+      aux = aux->next;
+      if (aux->next == NULL){
+        fprintf(stderr, "%s\n", "Not existing element");
+        return -1;
+      }
+    }
+    prev->next = aux->next;
+    free(aux); //Hacemos free de la posición actual
+    return 1;
+  }
 }
