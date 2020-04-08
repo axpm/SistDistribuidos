78ecdab96be5f75e8a7cccf2af716e2b8caea2d1
apmpm <alejandrpm@gmail.com>
2020-04-08 14:41:56 +0200

    preparación del apartado 3 de la práctica

---
+++ practica1/upper/UpperPublisher.java
@@ -0,0 +1,12 @@
+package upper;
+
+import javax.xml.ws.Endpoint;
+
+//Clase para publicar el servicio 
+public class UpperPublisher {
+  public static void main(String[ ] args) {
+    final String url = "http://localhost:8888/rs";
+    System.out.println("Publishing UpperService at endpoint " + url);
+    Endpoint.publish(url, new UpperService());
+  }
+}
---
+++ practica1/upper/UpperService.java
@@ -0,0 +1,14 @@
+package upper;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import java.util.Random;
+
+  @WebService
+  public class UpperService {
+
+    @WebMethod //Es un servicio Web para --> client
+    public String standardizeString(String input) { //devuelve el String input, pero en mayúsculas
+      return input.toUpperCase();
+    }
+}
